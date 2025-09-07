81ad7651d5126224a4994a259446fad35d7b8025
ShrekCoding21 <170429124+ShrekCoding21@users.noreply.github.com>
2025-09-06 22:52:08 -0400

    Updated cache

---
+++ Assets/Scripts/NewMonoBehaviourScript.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class NewMonoBehaviourScript : MonoBehaviour
+{
+    public GameObject publicObject;
+    [SerializeField] private GameObject serializedObject;
+    private GameObject privateObject;
+}
+
+
+
+
+
+
